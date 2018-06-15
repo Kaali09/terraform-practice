77c0f388a1d740d2c9f91b917505fc2528f53918
kaliraja.ramasamy <kaliraja.ramasamy@tarento.com>
2018-06-15 18:12:07 +0530

    added terraform files

---
+++ test-tf-project/variables.tf
@@ -0,0 +1,8 @@
+variable region {
+default = "ap-southeast-1"
+ description = "the aws region where we want to create the resources"
+}
+
+variable myvpcid { 
+default = "vpc-422e9925"
+}
---
+++ tf-module/variables.tf
@@ -0,0 +1,6 @@
+variable modname {}
+variable SubnetId {
+default = "subnet-7691e811"
+ }
+variable SecGroupId {}
+
